5fe90078c9bb72ea05b81197156b75ffba83e45a
SRI <srimurugharaj5901@gmail.com>
2024-12-26 15:41:20 +0530

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FaBars, FaTachometerAlt } from "react-icons/fa"; // Import desired icons
+
+function Navbar() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const sidebarRef = useRef(null);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+      setSidebarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isSidebarOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isSidebarOpen]);
+
+  return (
+    <>
+      <button
+        onClick={toggleSidebar}
+        type="button"
+        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      >
+        <FaBars className="w-6 h-6" />
+      </button>
+
+      <aside
+        ref={sidebarRef}
+        id="logo-sidebar"
+        className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } sm:translate-x-0 bg-[#f9f9f9]`}
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 py-4 overflow-y-auto">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center ps-2.5 mb-5"
+          >
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-6 me-3 sm:h-7"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-black">
+              Flowbite
+            </span>
+          </a>
+          <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+              >
+                <FaTachometerAlt className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" />
+                <span className="ms-3">Dashboard</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+    </>
+  );
+}
+
+export default Navbar;
